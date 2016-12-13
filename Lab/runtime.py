813db476f5780b13399a1e247b9cf3adb926cba2
Anung Ariwibowo <barliant@gmail.com>
2016-12-14 05:51:57 +0700

    Lab works.

--- Lab/runtime.py
+++ Lab/runtime.py
@@ -1,16 +1,19 @@
 import timing
 
 count = 10**6
-nums = []
+
 timing.log('Start append')
+nums = []
 for i in range(count):
     nums.append(i)
 nums.reverse()
 timing.endlog()
+print nums[:20]
 
 timing.log('Start insert')
 nums = []
 for i in range(count):
     nums.insert(0, i)
 timing.endlog()
+print nums[:20]
 
